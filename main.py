91dbc98a5ecbff235a8fdf32274a6379e7c8904c
Griffin Brome <griffinbrome@gmail.com>
2020-09-12 11:33:04 -0700

    Initial Assignment Setup
    Creat small python project, installed pytest for testing later. Add
    small .gitignore to keep annoying files out of the way.

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+COSC 499 Toy Git Assignment
+Author: Griffin Brome | 63057285
+
+This is a simple python program that takes a list of integers and sorts them
+"""
+
+sort_list(list):
+    """
+    Takes a list of length 'n' of integers as input and sorts them
+
+    This is done using Merge Sort, more information on the algorithm can be 
+    found here https://www.geeksforgeeks.org/merge-sort/
+    """ 
+    pass
